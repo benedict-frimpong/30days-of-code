c8bbfb7c4cb0f770c018dbcf0496d32b015a1c50
Benedict Frimpong <benedict.frimpong@udc.edu>
2023-09-27 18:13:21 -0400

    answer day1 problem

--- day1.java
+++ day1.java
@@ -4,21 +4,21 @@ Today, we're discussing data types. Check out the Tutorial tab for learning mate
 
 Task
 Complete the code in the editor below. The variables i, d, and s are already declared and initialized for you. You must:
-Declare  variables: one of type int, one of type double, and one of type String.
-Read  lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your variables.
-Use the  operator to perform the following operations:
-Print the sum of  plus your int variable on a new line.
-Print the sum of  plus your double variable to a scale of one decimal place on a new line.
-Concatenate  with the string you read as input and print the result on a new line.
+Declare 3 variables: one of type int, one of type double, and one of type String.
+Read 3 lines of input from stdin (according to the sequence given in the Input Format section below) and initialize your variables.
+Use the + operator to perform the following operations:
+Print the sum of i plus your int variable on a new line.
+Print the sum of d plus your double variable to a scale of one decimal place on a new line.
+Concatenate s with the string you read as input and print the result on a new line.
 Note: If you are using a language that doesn't support using  for string concatenation (e.g.: C), you can just print one variable immediately following the other on the same line. The string provided in your editor must be printed first, immediately followed by the string you read as input.
 
 Input Format
-The first line contains an integer that you must sum with .
-The second line contains a double that you must sum with .
-The third line contains a string that you must concatenate with .
+The first line contains an integer that you must sum with i.
+The second line contains a double that you must sum with d.
+The third line contains a string that you must concatenate with s.
 
 Output Format
-Print the sum of both integers on the first line, the sum of both doubles (scaled to  decimal place) on the second line, and then the two concatenated strings on the third line.
+Print the sum of both integers on the first line, the sum of both doubles (scaled to 1 decimal place) on the second line, and then the two concatenated strings on the third line.
 Sample Input
 12
 4.0
@@ -34,6 +34,39 @@ When we sum the floating-point numbers 4.0 and 4.0, we get 8.0.
 When we concatenate HackerRank with is the best place to learn and practice coding!, we get HackerRank is the best place to learn and practice coding!.
 You will not pass this challenge if you attempt to assign the Sample Case values to your variables instead of following the instructions above and reading input from stdin.
  */
+
+import java.util.Scanner;
+
 public class day1 {
-    
+     public static void main(String[] args) {
+        int i = 4;
+        double d = 4.0;
+        String s = "HackerRank ";
+		
+        Scanner scan = new Scanner(System.in);
+
+ /* Declare second integer, double, and String variables. */
+        int i2 ;
+        double d2  ;
+        String s2 ;
+
+/* Read and save an integer, double, and String to your variables.*/
+        i2 = scan.nextInt();
+        d2 = scan.nextDouble();
+        scan.nextLine();
+        s2 = scan.nextLine();
+        
+// Note: If you have trouble reading the entire String, please go back and review the Tutorial closely.
+        
+/* Print the sum of both integer variables on a new line. */
+        System.out.println(i + i2);
+/* Print the sum of the double variables on a new line. */
+		System.out.println(d + d2);
+
+/* Concatenate and print the String variables on a new line; 
+the 's' variable above should be printed first. */
+        System.out.println(s + s2);
+        
+        scan.close(); 
+     }
 }
