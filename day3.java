875f61b9534801ee2e8c2a84119b3480abd914d9
Benedict Frimpong <benedict.frimpong@udc.edu>
2023-09-30 05:41:23 -0400

    solve day3 problem

--- day3.java
+++ day3.java
@@ -53,6 +53,19 @@ public class day3 {
 
         int N = Integer.parseInt(bufferedReader.readLine().trim());
 
+        if (N % 2 != 0){
+            System.out.println("Weird");
+        }
+        else if (N % 2 == 0 && (N >= 2 && N <= 5)){
+            System.out.println("Not Weird");
+        }
+        else if (N % 2 == 0 && (N >= 6 && N <= 20)){
+            System.out.println("Weird");
+        }
+        else if (N % 2 == 0 && N > 20){
+            System.out.println("Not Weird");
+        }
+
         bufferedReader.close();
     }
 }
